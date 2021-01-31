51a85ccf0df6779cfa79c7a9d2e50be55aba934e
grieey <yangqing433520@163.com>
2021-01-31 14:57:45 +0800

    添加协程的依赖

--- app/src/main/java/com/github/grieey/coreschedule/MainActivity.kt
+++ app/src/main/java/com/github/grieey/coreschedule/MainActivity.kt
@@ -3,13 +3,22 @@ package com.github.grieey.coreschedule
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.grieey.core_schedule.scheduleOnIO
+import com.github.grieey.core_schedule.scheduleOnMain
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     scheduleOnIO(1000) {
-      // do something
+      // do something on Io thread
+    }
+    scheduleOnMain(1000) {
+      // do something on Main thread
+    }
+    GlobalScope.launch {
+
     }
   }
 }
--- core-schedule/src/main/java/com/github/grieey/core_schedule/ScheduleExt.kt
+++ core-schedule/src/main/java/com/github/grieey/core_schedule/ScheduleExt.kt
@@ -1,5 +1,9 @@
 package com.github.grieey.core_schedule
 
+import android.os.Handler
+import android.os.Looper
+import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.GlobalScope
 import java.util.*
 import kotlin.concurrent.schedule
 
@@ -8,8 +12,18 @@ import kotlin.concurrent.schedule
  * @date: 2021/1/31 14:14
  * @author: Grieey
  */
+/**
+ * 延迟delay的时间执行block
+ */
 inline fun scheduleOnIO(delay: Long, crossinline block: () -> Unit) {
-    Timer().schedule(delay) {
-        block()
-    }
+  Timer().schedule(delay) {
+    block()
+  }
+}
+
+/**
+ * 延迟delay的时间在主线程执行block
+ */
+inline fun scheduleOnMain(delay: Long, crossinline block: () -> Unit) {
+  Handler(Looper.getMainLooper()).postDelayed({ block() }, delay)
 }
